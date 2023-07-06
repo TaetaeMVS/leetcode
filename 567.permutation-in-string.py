24ad053e3f90a0df5eee29fc3bea8619c1bbaa84
Tae's PC <53635845+taeyeong2k@users.noreply.github.com>
2023-07-06 17:08:18 +1000

    update solutions

---
+++ 567.permutation-in-string.py
@@ -0,0 +1,85 @@
+#
+# @lc app=leetcode id=567 lang=python3
+#
+# [567] Permutation in String
+#
+# https://leetcode.com/problems/permutation-in-string/description/
+#
+# algorithms
+# Medium (44.23%)
+# Likes:    10089
+# Dislikes: 323
+# Total Accepted:    674.5K
+# Total Submissions: 1.5M
+# Testcase Example:  '"ab"\n"eidbaooo"'
+#
+# Given two strings s1 and s2, return true if s2 contains a permutation of s1,
+# or false otherwise.
+# 
+# In other words, return true if one of s1's permutations is the substring of
+# s2.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: s1 = "ab", s2 = "eidbaooo"
+# Output: true
+# Explanation: s2 contains one permutation of s1 ("ba").
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: s1 = "ab", s2 = "eidboaoo"
+# Output: false
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= s1.length, s2.length <= 10^4
+# s1 and s2 consist of lowercase English letters.
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2): return False
+        
+        s1Count, s2Count = [0] * 26, [0] * 26
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
+        
+        matches = 0
+        for i in range(26):
+            matches += (1 if s1Count[i] == s2Count[i] else 0)
+            
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26: return True
+            
+            index = ord(s2[r]) - ord('a')
+            s2Count[index] += 1
+            if s2Count[index] == s1Count[index]:
+                matches += 1
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches -= 1
+            
+            index = ord(s2[l]) - ord('a')
+            s2Count[index] -= 1
+            if s2Count[index] == s1Count[index]:
+                matches += 1
+            elif s1Count[index] - 1 == s2Count[index]:
+                matches -= 1
+                
+            l += 1
+        return matches == 26
+            
+            
+# @lc code=end
+
