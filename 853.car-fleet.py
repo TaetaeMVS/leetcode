7e3e7e0f3ddf5a6a9ea6170ac0b3addf7a209841
Tae Yeong Kim <53635845+taeyeong2k@users.noreply.github.com>
2024-02-06 14:47:57 +1100

    car fleet

--- 853.car-fleet.py
+++ 853.car-fleet.py
@@ -89,11 +89,8 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         cars = [[p, s] for p, s in zip(position, speed)]
-        cars.sort()
-        cars.reverse()
-        
         stack = []
-        for p, s in cars:
+        for p, s in sorted(cars)[::-1]:
             stack.append((target - p) / s)
             
             if len(stack) > 1 and stack[-1] <= stack[-2]:
