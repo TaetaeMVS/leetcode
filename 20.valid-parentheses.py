770e50de2b51dca3acff13c7f5f727e69b63f134
Tae Yeong Kim <53635845+taeyeong2k@users.noreply.github.com>
2024-02-06 11:42:41 +1100

    valid parenthesis

---
+++ 20.valid-parentheses.py
@@ -0,0 +1,76 @@
+#
+# @lc app=leetcode id=20 lang=python3
+#
+# [20] Valid Parentheses
+#
+# https://leetcode.com/problems/valid-parentheses/description/
+#
+# algorithms
+# Easy (40.33%)
+# Likes:    23221
+# Dislikes: 1634
+# Total Accepted:    4.2M
+# Total Submissions: 10.5M
+# Testcase Example:  '"()"'
+#
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and
+# ']', determine if the input string is valid.
+# 
+# An input string is valid if:
+# 
+# 
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+# 
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: s = "()"
+# Output: true
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: s = "()[]{}"
+# Output: true
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: s = "(]"
+# Output: false
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= s.length <= 10^4
+# s consists of parentheses only '()[]{}'.
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {"(": ")", "{": "}", "[": "]"}
+        for char in s:
+            if char in mapping.keys():
+                stack.append(mapping[char])
+            else:
+                if stack and char == stack.pop():
+                    continue
+                return False
+                
+        if stack:
+            return False
+        return True
+# @lc code=end
+
