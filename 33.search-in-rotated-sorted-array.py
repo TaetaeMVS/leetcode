bfad3fe446c0b8ab8fd67de5ad975fcd25d0ee27
Tae Yeong Kim <53635845+taeyeong2k@users.noreply.github.com>
2024-02-08 23:45:39 +1100

    33. search in rotated sorted array

---
+++ 33.search-in-rotated-sorted-array.py
@@ -0,0 +1,98 @@
+#
+# @lc app=leetcode id=33 lang=python3
+#
+# [33] Search in Rotated Sorted Array
+#
+# https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+#
+# algorithms
+# Medium (40.25%)
+# Likes:    25386
+# Dislikes: 1507
+# Total Accepted:    2.6M
+# Total Submissions: 6.3M
+# Testcase Example:  '[4,5,6,7,0,1,2]\n0'
+#
+# There is an integer array nums sorted in ascending order (with distinct
+# values).
+# 
+# Prior to being passed to your function, nums is possibly rotated at an
+# unknown pivot index k (1 <= k < nums.length) such that the resulting array is
+# [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]
+# (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3
+# and become [4,5,6,7,0,1,2].
+# 
+# Given the array nums after the possible rotation and an integer target,
+# return the index of target if it is in nums, or -1 if it is not in nums.
+# 
+# You must write an algorithm with O(log n) runtime complexity.
+# 
+# 
+# Example 1:
+# Input: nums = [4,5,6,7,0,1,2], target = 0
+# Output: 4
+# Example 2:
+# Input: nums = [4,5,6,7,0,1,2], target = 3
+# Output: -1
+# Example 3:
+# Input: nums = [1], target = 0
+# Output: -1
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= nums.length <= 5000
+# -10^4 <= nums[i] <= 10^4
+# All values of nums are unique.
+# nums is an ascending array that is possibly rotated.
+# -10^4 <= target <= 10^4
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            val = nums[mid]
+            if val == target:
+                return mid
+            if val >= nums[l]:
+                # left sorted portion, and target is larger than val
+                # therefore target must be to the right of val
+                if target > val:
+                    l = mid + 1
+                
+                # left sorted portion and target is less than val
+                else:
+                    if target < nums[l]:
+                        l = mid + 1
+                    else:
+                        r = mid - 1
+                        
+                    
+            # right sorted portion
+            elif val < nums[l]:
+                # right sorted portion, and target > val
+                # [6, 1, 2, 3, 5]  t = 1
+                if target > val:
+                    if target <= nums[r]:
+                        l = mid + 1
+                
+                    else:
+                        r = mid - 1
+                    
+                elif target < val:
+                    r = mid - 1
+            else:
+                return -1
+            
+                    
+        return -1
+                
+            
+# @lc code=end
+
